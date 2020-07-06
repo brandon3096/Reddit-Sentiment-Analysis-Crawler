c3c05cffa06422828ad60db3074522195d77f386
brandon3096 <broyer@umich.edu>
2020-07-06 19:14:01 -0400

    Indefinitely parse forest of comments

--- Crawler/Crawler.py
+++ Crawler/Crawler.py
@@ -4,13 +4,18 @@ import os
 if __name__ == "__main__":    
     reddit = praw.Reddit('RedditSentimentAnalysisBot')
 
-    postIds = []
+    postIds = {}
     comments = []
 
     for submission in reddit.subreddit('me_irl').stream.submissions():   
+        postIds[submission.id] = True
         submission.comments.replace_more(limit=None)
-        for comment in submission.comments.list():
+        comment_queue = submission.comments[:]  # Seed with top-level
+        while comment_queue:
+            comment = comment_queue.pop(0)
             print(comment.body)
-            #comments.append(comment)
+            comments.append(comment)
+            comment_queue.extend(comment.replies)
 
-    print("BEANS")
+    print(postIds)
+    print(comments)
