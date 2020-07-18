76a24c218279023d4cba95ce009a82b7b0e09d8a
brandon3096 <broyer@umich.edu>
2020-07-18 19:27:01 -0400

    identify comments containing a stock ticker and write to db

--- Crawler/reddit_crawler_main.py
+++ Crawler/reddit_crawler_main.py
@@ -12,26 +12,25 @@ if __name__ == "__main__":
     postIds = {}
     comments = []
     handler = mysql_queryhandler.queryhandler(50)
-    count = 0    
 
     # Get tracked stocks
     stocks = handler.get_valid_stocks()
 
     # Process comment stream
     try:
-        for submission in reddit.subreddit('me_irl').stream.submissions():   
-            print("New submission")
+        for submission in reddit.subreddit('wallstreetbets').stream.submissions():   
             postIds[submission.id] = True
             submission.comments.replace_more(limit=None)
             comment_queue = submission.comments[:]  # Seed with top-level
             while comment_queue:
-                comment = comment_queue.pop(0)                
-                insert_comment = ("TEST"+str(count), "Reddit", random.randrange(1, 1000), datetime.now())
-                handler.add_to_queue(insert_comment)
+                comment = comment_queue.pop(0)    
+                for x in comment.body.split(' '):
+                    if x in stocks:
+                        insert_comment = (x, "Reddit", random.randrange(1, 1000), datetime.now())
+                        handler.add_to_queue(insert_comment)
+                        print("Found mention of {} in {}".format(x, comment.body))
                 comments.append(comment)
                 comment_queue.extend(comment.replies)
-                print("Push to queue: ", count)
-                count += 1
     except KeyboardInterrupt:
         del handler
 
